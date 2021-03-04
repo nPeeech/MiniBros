8dc727ed48a502ad037b820c1ac8f8a6d18d159d
nPeeech <33167904+nPeeech@user.noreply.github.com>
2021-03-04 23:08:54 +0900

    wasdでコマンドが送信されるようにした

--- main.js
+++ main.js
@@ -47,3 +47,76 @@ document.querySelector('#send1').onclick = () => {
     sendData("1");
     console.log("send 1");
 }
+
+// 移動コマンド
+function goStraight() {
+    sendData("a");
+    console.log("send a");
+}
+function goBack() {
+    sendData("b");
+    console.log("send b");
+}
+function turnRight() {
+    sendData("c");
+    console.log("send c");
+}
+function turnLeft() {
+    sendData("d");
+    console.log("send d");
+}
+function stopMoving() {
+    sendData("e");
+    console.log("send e");
+}
+
+// キー長押し時の連続入力を抑制する
+let prevus_down_key_code = null;
+function throttle_back_key_down(code, func) {
+    if (prevus_down_key_code != code) {
+        prevus_down_key_code = code;
+        func();
+    }
+}
+function throttle_back_key_up(code, func) {
+    if (prevus_down_key_code === code) {
+        prevus_down_key_code = null;
+    }
+    func();
+}
+window.addEventListener("keydown", event => {
+    switch (event.code) {
+        case "KeyW":
+            throttle_back_key_down("KeyW", goStraight);
+            break;
+        case "KeyS":
+            throttle_back_key_down("KeyS", goBack);
+            break;
+        case "KeyD":
+            throttle_back_key_down("KeyD", turnRight);
+            break;
+        case "KeyA":
+            throttle_back_key_down("KeyA", turnLeft);
+            break;
+        default:
+            break;
+    }
+});
+window.addEventListener("keyup", event => {
+    switch (event.code) {
+        case "KeyW":
+            throttle_back_key_up("KeyW", stopMoving);
+            break;
+        case "KeyS":
+            throttle_back_key_up("KeyS", stopMoving);
+            break;
+        case "KeyD":
+            throttle_back_key_up("KeyD", stopMoving);
+            break;
+        case "KeyA":
+            throttle_back_key_up("KeyA", stopMoving);
+            break;
+        default:
+            break;
+    }
+});
